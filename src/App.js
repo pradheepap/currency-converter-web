1f6ff6a33174d7805a615416b69c72cc942b1bad
Pradheepa P <pradheepa.p@gmail.com>
2022-03-21 11:54:37 +0800

    Added User info and analytics

--- src/App.js
+++ src/App.js
@@ -19,7 +19,11 @@ const [from, setFrom] = useState("USD");
 const [to, setTo] = useState("SGD");
 const [options, setOptions] = useState([]);
 const [output, setOutput] = useState(0);
-const [userName, setUserName] = useState([]);
+const [user, setUser] = useState("");
+const [count, setCount] = useState("");
+const [usdvalue, setUsdValue] = useState("");
+
+
 
 
 const EXCHANGE_RATES = `{  	
@@ -34,9 +38,19 @@ const client = new ApolloClient({
 	cache: new InMemoryCache(),
 	headers: {
 		'x-api-key': `da2-praw6gf5bff7rojzikysxrg2gm`,
-		'userName' : userName
+		'userName' : user
 	  }
   });
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("user");
+    if (loggedInUser) {
+      const foundUser = JSON.parse(loggedInUser);
+      setUser(foundUser);
+    }else{
+		setUser(`User_` + uuid());
+	}
+  }, []);
 
 // Calling the api whenever the dependency changes
 useEffect(() => {
@@ -101,6 +115,41 @@ function convert() {
   });	
 }
 
+
+// Function to convert the currency
+function getCount() { 
+	const TRANSACTIONS_COUNT = `{  	
+		getTransactionsCount
+	}`;
+	client
+  .query({
+    query: gql `${TRANSACTIONS_COUNT}`
+  })
+  .then( (result) => {
+	  console.log (result.data.getTransactionsCount);
+	  const getTransactionsCount = (result.data.getTransactionsCount);
+	  console.log ((getTransactionsCount));
+	  setCount(getTransactionsCount);
+  });	
+}
+
+// Function to convert the currency
+function getTotalValueInUsd() { 
+	const TRANSACTIONS_TOTAL_VALUE_IN_USD = `{  	
+		getValueInUSD
+	}`;
+	client
+  .query({
+    query: gql `${TRANSACTIONS_TOTAL_VALUE_IN_USD}`
+  })
+  .then( (result) => {
+	  console.log (result.data.getValueInUSD);
+	  const getValueInUSD = (result.data.getValueInUSD);
+	  console.log ((getValueInUSD));
+	  setUsdValue(getValueInUSD);
+  });	
+}
+
 // Function to switch between two currency
 // function flip() {
 // 	var temp = from;
@@ -113,6 +162,9 @@ return (
 	<div className="App">
 	<div className="heading">
 		<h1>Currency converter</h1>
+	</div>
+	<div className="userInfo">
+		<h1>Welcome {user} </h1>
 	</div>
 	<div className="container">
 		<div className="left">
@@ -142,6 +194,16 @@ return (
 		<button onClick={()=>{convert()}}>Convert</button>
 		<h2>Converted Amount:</h2>
 		<p>{input+" "+from+" = "+output.toFixed(2) + " " + to}</p>
+	</div>
+	<div className="count">
+		<button onClick={()=>{getCount()}}>Count</button>
+		<h2>Number of Transactions</h2>
+		<p>{ " Count : "+ count}</p>
+	</div>
+	<div className="totalvalue">
+		<button onClick={()=>{getTotalValueInUsd()}}>Total</button>
+		<h2>Total Value in USD</h2>
+		<p>{usdvalue}</p>
 	</div>
 	</div>
 );
